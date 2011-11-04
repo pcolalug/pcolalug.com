43157cf639f3b97e9ea4f5cc1502a2c4887cd0f0
John Anderson <sontek@gmail.com>
2011-11-04 13:33:13 -0500

    changed up the prodwsgi

--- prodwsgi.py
+++ prodwsgi.py
@@ -3,4 +3,4 @@ import os
 
 ROOT_PATH = os.path.dirname(__file__)
 
-application = get_app(os.path.join(ROOT_PATH, 'development.ini'), 'main')
+application = get_app(os.path.join(ROOT_PATH, 'production.ini'), 'main')
