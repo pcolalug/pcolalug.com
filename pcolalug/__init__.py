e0aefb013d916b837e71f8574dea4ebce2acc9de
John Anderson <sontek@gmail.com>
2011-11-04 14:47:35 -0500

    use unicode

--- pcolalug/__init__.py
+++ pcolalug/__init__.py
@@ -29,8 +29,8 @@ def main(global_config, **settings):
     else:
         from bundle_config import config
 
-        url = "postgresql+psycopg2://%(username)s:%(password)s@%(host)s/%(database)s" % config['postgres']
-        print url
+        url = u"postgresql+psycopg2://%(username)s:%(password)s@%(host)s/%(database)s" % config['postgres']
+        print "USING URL: %s" % url
         engine = create_engine(url)
 
     initialize_sql(engine)
